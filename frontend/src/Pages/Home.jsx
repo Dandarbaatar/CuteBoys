2cd4f4391571ea3d5993b1b8e300fbb522de300c
2022Hop2c-Orgil <orgil@iMac2021-08s-iMac.local>
2023-02-13 19:34:18 +0800

    no changes

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,8 +3,102 @@ import Header from "../component/header";
 
 export const Home = () => {
   return (
-    <div className="home">
-      <Header />
+    <div className="Homebig">
+      <div className="Homepage1">
+        <div className="'HomePage1Left">
+          <div className="HomePage1Up">
+            <div className="Homepage1Text1">
+              <div className="Homepage1Text">Innovation</div>
+              <div className="Homepage1Text">2 Hours ago</div>
+            </div>
+            <h1 className="Homepage1Text2">
+              Charge Two Devices at the Same Time With This Magnetic Wireless
+              Charging Dock
+            </h1>
+          </div>
+          <div className="HomePage1Cards">
+            <div className="HomePage1Card">
+              <div className="HomePage1CardImg"></div>
+              <div className="HomePage1CardText">
+                Charge Two Devices at the Same Time With This Magnetic Wireless
+                Charging Dock
+              </div>
+            </div>
+            <div className="HomePage1Card">
+              <div className="HomePage1CardImg"></div>
+              <div className="HomePage1CardText">
+                Tiny moon rover could be a stepping stone to Mars
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="HomePage1Right">
+          <div className="HomePage1RightText1">Popular this week</div>
+          <div className="HomePage1RightText2">
+            <div className="HomePage1RightText2Card">
+              <div className="HomePage1RightText2CardImg"></div>
+              <div className="HomePage1RightText2CardTexts">
+                <div className="Homepage1Text11">
+                  <div className="HomePage1RightText2CardText">Innovation</div>
+                  <div className="HomePage1RightText2CardText">2 Hours ago</div>
+                </div>
+                <div className="HomePage1RightText2CardText1">
+                  7 Ways You Can Reduce Climate Change
+                </div>
+              </div>
+            </div>
+            <div className="HomePage1RightText2Card">
+              <div className="HomePage1RightText2CardImg"></div>
+              <div className="HomePage1RightText2CardTexts">
+                <div className="Homepage1Text11">
+                  <div className="HomePage1RightText2CardText">Innovation</div>
+                  <div className="HomePage1RightText2CardText">2 Hours ago</div>
+                </div>
+                <div className="HomePage1RightText2CardText1">
+                  7 Ways You Can Reduce Climate Change
+                </div>
+              </div>
+            </div>
+            <div className="HomePage1RightText2Card">
+              <div className="HomePage1RightText2CardImg"></div>
+              <div className="HomePage1RightText2CardTexts">
+                <div className="Homepage1Text11">
+                  <div className="HomePage1RightText2CardText">Innovation</div>
+                  <div className="HomePage1RightText2CardText">2 Hours ago</div>
+                </div>
+                <div className="HomePage1RightText2CardText1">
+                  7 Ways You Can Reduce Climate Change
+                </div>
+              </div>
+            </div>
+            <div className="HomePage1RightText2Card">
+              <div className="HomePage1RightText2CardImg"></div>
+              <div className="HomePage1RightText2CardTexts">
+                <div className="Homepage1Text11">
+                  <div className="HomePage1RightText2CardText">Innovation</div>
+                  <div className="HomePage1RightText2CardText">2 Hours ago</div>
+                </div>
+                <div className="HomePage1RightText2CardText1">
+                  7 Ways You Can Reduce Climate Change
+                </div>
+              </div>
+            </div>{" "}
+            <div className="HomePage1RightText2Card">
+              <div className="HomePage1RightText2CardImg"></div>
+              <div className="HomePage1RightText2CardTexts">
+                <div className="Homepage1Text11">
+                  <div className="HomePage1RightText2CardText">Innovation</div>
+                  <div className="HomePage1RightText2CardText">2 Hours ago</div>
+                </div>
+                <div className="HomePage1RightText2CardText1">
+                  7 Ways You Can Reduce Climate Change
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Homepage2"></div>
     </div>
   );
 };
