73dcd1163a46ad723bef475b3a60c9fc430dc9b5
Munkhtengis <tengistengis0516@gmail.com>
2023-02-17 17:34:53 +0800

    Tengis 1

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./component/footer";
+import Author from "./Pages/Author";
+
+function App() {
+  return (
+    <div className="App">
+      <Author />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
