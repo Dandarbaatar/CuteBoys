c42b833dfd1cf2909d1728dba4a197142ccf1eb7
Munkhtengis <tengistengis0516@gmail.com>
2023-02-17 19:53:49 +0800

    Tengis 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home";
+import Footer from "./component/footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/footer" element={<Footer />} />
         {/* <Route path="/search" element={<Search />} /> */}
       </Routes>
     </BrowserRouter>
