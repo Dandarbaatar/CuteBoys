32edcacde28e506e349ae55992efc6e2a0beb275
2022Hop2c-Chingun <chingun-33@Mandakh.local>
2023-02-27 18:20:08 +0800

    page iin tovch saijruulsan bas search buttonii zurgiig svg bolgoson

--- frontend/src/component/changepage.jsx
+++ frontend/src/component/changepage.jsx
@@ -1,24 +1,50 @@
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/esm/Button";
 
-export const Changepage = () => {
+export const Changepage = ({ color, setColor, el, one, setOne }) => {
+  const [imp, setImp] = useState(false);
+  const number = el + 1;
+  useEffect(() => {
+    if (number === 1) {
+      setImp(true);
+      setColor(false);
+      setOne(true);
+    }
+  }, [number]);
+  const Change = () => {
+    if (number === 1) {
+      setOne(true);
+    }
+    if (imp) {
+      setOne(false);
+      setImp(false);
+      setColor(true);
+    }
+    if (color) {
+      setOne(false);
+      setImp(true);
+      setColor(false);
+    }
+  };
   return (
     <div>
       <Button
+        onClick={Change}
         style={{
           display: "flex",
           alignItems: "center",
           border: "none",
-          borderRadius: "12px",
+          borderRadius: "0.5vh",
           justifyContent: "center",
           marginRight: "0.5vw",
-          fontSize: "0.7vw",
-          color: "white",
-          height: "2.5vw",
-          width: "2.5vw",
-          backgroundColor: "#109BE9",
+          fontSize: "0.85vw",
+          color: imp ? "white" : "black",
+          height: "2vw",
+          width: "2vw",
+          backgroundColor: imp ? "#109BE9" : "transparent",
         }}
       >
-        1
+        {number}
       </Button>
     </div>
   );
